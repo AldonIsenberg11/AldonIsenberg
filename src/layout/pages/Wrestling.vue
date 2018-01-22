65fad79246f1ef1c99d214afe2633b654d0844f4
Aldon <aisenberg@freightview.com>
2018-01-21 20:57:19 -0600

    add funny pic of wrestling

--- src/layout/pages/Wrestling.vue
+++ src/layout/pages/Wrestling.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Wrestling</div>
+  <div class="container">
+    <h1 class="title">Wrestling</h1>
+    <h2>Haha that's cold blooded LOL</h2>
+    <img src="./img/hahaNolf.png" alt="Should be pic here" />
+  </div>
 </template>
