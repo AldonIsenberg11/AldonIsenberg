5a14dd0126e8ce4baa96e8844d7dff23fee9866b
Aldon <aisenberg@freightview.com>
2019-01-04 17:40:30 -0600

    this is a test

--- src/layout/pages/Home.vue
+++ src/layout/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="title">Home</h1>
+    <h1 class="title">Homie</h1>
     <img src="./img/coverPic.png" alt="Should be pic here" />
   </div>
 </template>
