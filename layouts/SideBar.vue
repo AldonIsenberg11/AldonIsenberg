6a5128e203cafd5016c7ccd4428d6379fee2ed8f
Aldon <aisenberg@freightview.com>
2020-05-05 16:00:07 -0500

    more on landing page

---
+++ layouts/SideBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <aside class="menu">
+      <p class="menu-label">
+        About Me
+      </p>
+      <ul class="menu-list">
+        <li><a>My Family</a></li>
+        <li><a>Developer</a></li>
+        <li><a>Education</a></li>
+        <li><a>Wrestling</a></li>
+        <li><a>Hobbies</a></li>
+
+      </ul>
+      <p class="menu-label">
+        Resume
+      </p>
+      <ul class="menu-list">
+        <li>
+          <a class="is-active">Work Experience</a>
+          <ul>
+            <li><a>Members</a></li>
+            <li><a>Plugins</a></li>
+            <li><a>Add a member</a></li>
+          </ul>
+        </li>
+        <li>
+          <a class="">Education</a>
+          <ul>
+            <li><a>Members</a></li>
+            <li><a>Plugins</a></li>
+            <li><a>Add a member</a></li>
+          </ul>
+        </li>
+        <li><a>Invitations</a></li>
+        <li><a>Cloud Storage Settings</a></li>
+        <li><a>Authentication</a></li>
+      </ul>
+      <p class="menu-label">
+        Transactions
+      </p>
+      <ul class="menu-list">
+        <li><a>Payments</a></li>
+        <li><a>Transfers</a></li>
+        <li><a>Balance</a></li>
+      </ul>
+    </aside>
+  </div>
+</template>
