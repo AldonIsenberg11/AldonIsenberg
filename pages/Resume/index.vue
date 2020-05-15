cc3f808a797f37106a9439d2605a58675b1e4dcc
Aldon <aisenberg@freightview.com>
2020-05-15 11:21:47 -0500

    update resume

--- pages/Resume/index.vue
+++ pages/Resume/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class='container'>
 
-
-    <section class="hero green-background green-background-text">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            Leadership
-          </h1>
-          <h2 class="subtitle">
-            Never ask others to do what you are unwilling to do yourself.
-          </h2>
+    <section class="container content green-background green-background-text">
+      <div class="container">&nbsp;</div>
+      <div class="container columns">
+        <div class="column is-one-quarter">
+          <h1 class="is-size-2 ">&nbsp;&nbsp;Work Experience</h1>
+        </div>
+        <div class="is-hidden-mobile">
+          <div class="container"></div>
+          <h2 class="is-size-4 is-italic">Never ask others to do what you are unwilling to do yourself.</h2>
+        </div>
+        <div class="column is-one-fifth ">
+          &nbsp;&nbsp;&nbsp;&nbsp;<a class="rose-text rose-underline-text hover-rose-underline-text is-size-3" href="/AI-resume.pdf" download>Download</a><span class="text-size-smaller">(PDF)</span>
         </div>
       </div>
+
     </section>
 
     <div class="container">&nbsp;</div>
@@ -23,29 +26,36 @@
           Freightview
         </h1>
         <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;Software Developer
+          &nbsp;&nbsp;&nbsp;&nbsp;Software&nbsp;Developer
         </h2>
         <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2017 to Present (3 years)
+        </p>
+        <h2 class="rose-text is-size-4">
+          &nbsp;&nbsp;&nbsp;&nbsp;Customer&nbsp;Support
+        </h2>
+        <p>
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to 2017 (1 year)
         </p>
       </div>
       <div class="container column green-overline">
         <div class="container">&nbsp;</div>
         <p class="content dark-background-text bullet-style text-spaced">
-          Freightview is a shipment management <span class="rose-text">software</span> that connects shippers to their carriers <span class="green-text">API's</span>. <br>
-          We use a <span class="green-text">CI/CD</span> <span class="rose-text">pipeline</span> which allows us to <span class="green-text">deploy</span> to production several times a day. <br>
+          Freightview is a shipment management <span class="green-text">software</span> that connects shippers to their carriers <span class="green-text">API's</span>. <br>
+          We use a <span class="green-text">CI/CD</span> <span class="rose-text">pipeline</span> which allows us to <span class="rose-text">deploy</span> to production several times a day. <br>
           I've worked several parts of our app, including but not limited to <span class="rose-text">implementing</span> new <span class="green-text">features</span> to the UI,
-          <span class="rose-text">integrating</span> external <span class="green-text">API's</span>, <span class="rose-text">developing</span> <span class="green-text">aggregation</span> queries from the DB, <span class="rose-text">debugging</span> expedited concerns,
-          <span class="rose-text">review</span><span class="green-text">/</span><span class="rose-text">approve</span> code for <span class="green-text">production</span> merge.
+          <span class="rose-text">integrating</span> external <span class="green-text">API's</span>, <span class="rose-text">developing</span> <span class="green-text">aggregation</span> queries from the DB,
+          <span class="rose-text">debugging</span> expedited concerns, and
+          <span class="rose-text">review</span><span class="green-text">/</span><span class="rose-text">approve</span> code for <span class="rose-text">production</span>.
           <ul class="is-size-6">
-            <li>AWS/Heroku - document storage and app hosting</li>
-            <li>NodeJS/ExpressJS - powers the back-end</li>
-            <li>MongoDB/Mongoose - primary database</li>
-            <li>Docker - connects microservices for easier development</li>
-            <li>ReactJS/BackboneJS - front-end framework</li>
-            <li>RabbitMQ - messaging client</li>
-            <li>NATS - streaming service</li>
-            <li>Redis - pub-sub service</li>
+            <li><span class="green-text">AWS/Heroku</span> - document storage and app hosting</li>
+            <li><span class="green-text">NodeJS/ExpressJS</span> - powers the back-end</li>
+            <li><span class="green-text">MongoDB/Mongoose</span> - primary database</li>
+            <li><span class="green-text">Docker</span> - connects microservices for easier development</li>
+            <li><span class="green-text">ReactJS/BackboneJS</span> - front-end framework</li>
+            <li><span class="green-text">RabbitMQ</span> - messaging client</li>
+            <li><span class="green-text">NATS</span> - streaming service</li>
+            <li><span class="green-text">Redis</span> - pub-sub service</li>
           </ul>
         </p>
       </div>
@@ -59,16 +69,34 @@
           Scott Logistics
         </h1>
         <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;Logistics Coordinator
+          &nbsp;&nbsp;&nbsp;&nbsp;Tracking&nbsp;Supervisor
         </h2>
         <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2015 to 2016 (10 months)
+        </p>
+        <h2 class="rose-text is-size-4">
+          &nbsp;&nbsp;&nbsp;&nbsp;Carrier&nbsp;Relations
+        </h2>
+        <p>
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2015 (6 months)
+        </p>
+        <h2 class="rose-text is-size-4">
+          &nbsp;&nbsp;&nbsp;&nbsp;Logistics&nbsp;Coordinator
+        </h2>
+        <p>
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2014 to 2015 (7 months)
         </p>
       </div>
       <div class="container column green-overline">
         <div class="container">&nbsp;</div>
         <p class="content dark-background-text bullet-style text-spaced">
-          I did work
+          <ul>
+            <li>Aided in the <span class='rose-text'>development</span> of business processes to decouple load matching & tracking freight</li>
+            <li><span class='rose-text'>Led</span> a team of <span class='green-text'>6</span> to ensure that we had <span class='green-text'>visibility</span> on all shipments to meet our <span class="rose-text">metrics</span></li>
+            <li><span class='rose-text'>Evaluated</span> & <span class='rose-text'>qualified</span> <span class='green-text'>1000+</span> Common & Contract Property Carriers via USDOT to do business with</li>
+            <li><span class='rose-text'>Achieved</span> an average of over <span class='green-text'>22% profit</span> <span class='rose-text'>margin</span> on all brokered loads during time as a Logistics Coordinator</li>
+            <li><span class='rose-text'>Generated</span> over <span class='green-text'>$100,000</span> in gross profit as a Logistics Coordinator within 5 months of hire</li>
+          </ul>
         </p>
       </div>
     </section>
@@ -84,13 +112,25 @@
           &nbsp;&nbsp;&nbsp;&nbsp;Auditor
         </h2>
         <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2013 to 2014 (1 years)
+        </p>
+        <h2 class="rose-text is-size-4">
+          &nbsp;&nbsp;&nbsp;&nbsp;Room&nbsp;Service
+        </h2>
+        <p>
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2012 to 2013 (1 years)
         </p>
       </div>
       <div class="container column green-overline">
         <div class="container">&nbsp;</div>
         <p class="content dark-background-text bullet-style text-spaced">
-          I did work
+          <ul>
+            <li><span class='rose-text'>Analyzed</span> & <span class='rose-text'>audited</span> end of day <span class='green-text'>reports</span> for the hotel, restaurant, and banquet departments</li>
+            <li><span class='rose-text'>Compiled</span> daily electronic <span class='green-text'>backups</span> of Marriott confidential information</li>
+            <li><span class='rose-text'>Detected</span> & <span class='rose-text'>corrected</span> mistakes if any on market code <span class='green-text'>assignments</span> and room <span class='green-text'>rates</span></li>
+            <li><span class='rose-text'>Performed</span> <span class='green-text'>bi-weekly</span> <span class='rose-text'>ordering</span> of sundries and <span class='green-text'>refreshments</span> for front lobby <span class='green-text'>marketplace</span></li>
+            <li>Room Service <span class='rose-text'>achieved</span> a rank of <span class='green-text'>5/312</span> in overall <span class='green-text'>customer satisfaction</span> for all Marriott locations</li>
+          </ul>
         </p>
       </div>
     </section>
@@ -103,98 +143,88 @@
           KPJ Inc. - CPA
         </h1>
         <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;Tax internship
+          &nbsp;&nbsp;&nbsp;&nbsp;Tax&nbsp;internship
         </h2>
         <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
+          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2014 (4 months)
         </p>
       </div>
       <div class="container column green-overline green-underline">
         <div class="container">&nbsp;</div>
         <p class="content dark-background-text bullet-style text-spaced">
-          I did work
+          <ul>
+            <li><span class='rose-text'>Scheduled</span> appointments, <span class='rose-text'>answered</span> phone calls and <span class='rose-text'>advised</span> <span class='green-text'>CPA’s</span> of their schedule</li>
+            <li><span class='rose-text'>Assembled</span> customer <span class='green-text'>tax</span> returns and <span class='rose-text'>notified</span> customers of completed returns</li>
+            <li><span class='rose-text'>Organized</span> and <span class='rose-text'>maintained</span> the client file room in an <span class='green-text'>organized</span> fashion</li>
+          </ul>
         </p>
       </div>
     </section>
+
+    <!-- <div class="container content is-hidden-mobile">&nbsp;</div> -->
+
+    <section class="container content green-background green-background-text">
+      <div class="container">&nbsp;</div>
+      <div class="container columns">
+        <div class="column is-narrow">
+          <h1 class="is-size-2 ">&nbsp;&nbsp;Education</h1>
+        </div>
+        <div class="is-hidden-mobile">
+          <div class="container"></div>
+          <h2 class="is-size-4 is-italic">B.A. in Sport Business | M.A. in Accounting</h2>
+        </div>
+      </div>
+      <!-- <div class="container">&nbsp;</div> -->
+    </section>
+
+    <!-- <div class="container">&nbsp;</div> -->
+
+    <div class="container columns">
+
+      <section class="dark-background dark-background-section column">
+        <div class="container">
+          <h1 class="green-underline-text is-size-3 text-spaced">
+            Shorter University
+          </h1>
+          <h2 class="rose-text is-size-4">
+            &nbsp;&nbsp;&nbsp;&nbsp;M.A.c.c Accounting
+          </h2>
+          <p>
+            <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2014 to 2016 (4 years)
+          </p>
+        </div>
+      </section>
+
+      <section class="dark-background dark-background-section column">
+        <div class="container">
+          <h1 class="green-underline-text is-size-3 text-spaced">
+            University of Iowa
+          </h1>
+          <h2 class="rose-text is-size-4">
+            &nbsp;&nbsp;&nbsp;&nbsp;B.A. Sports Business
+          </h2>
+          <p>
+            <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2011 to 2014 (3 years)
+          </p>
+        </div>
+      </section>
+
+      <section class="dark-background dark-background-section column">
+        <div class="container">
+          <h1 class="green-underline-text is-size-3 text-spaced">
+            Fort Hays State University
+          </h1>
+          <h2 class="rose-text is-size-4">
+            &nbsp;&nbsp;&nbsp;&nbsp;Management Information Systems
+          </h2>
+          <p>
+            <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2009 to 2011 (2 years)
+          </p>
+        </div>
+      </section>
+    </div>
 
     <div class="container">&nbsp;</div>
-
-    <section class="hero green-background green-background-text">
-      <div class="hero-body">
-        <h1 class="title">
-          Education
-        </h1>
-        <h2 class="subtitle">
-          B.A. in Sport Business | M.A. in Accounting
-        </h2>
-      </div>
-    </section>
-
-    <div class="container">&nbsp;</div>
-
-    <section class="dark-background dark-background-section columns">
-      <div class="container column is-one-third">
-        <h1 class="green-underline-text is-size-3 text-spaced">
-          Shorter University
-        </h1>
-        <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;M.A.c.c Accounting
-        </h2>
-        <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
-        </p>
-      </div>
-      <div class="container column">
-        <div class="container">&nbsp;</div>
-        <p class="content dark-background-text bullet-style text-spaced">
-          I did work
-        </p>
-      </div>
-    </section>
-
-    <div class="container">&nbsp;</div>
-
-    <section class="dark-background dark-background-section columns">
-      <div class="container column is-one-third">
-        <h1 class="green-underline-text is-size-3 text-spaced">
-          University of Iowa
-        </h1>
-        <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;B.A. Sports Business
-        </h2>
-        <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
-        </p>
-      </div>
-      <div class="container column">
-        <div class="container">&nbsp;</div>
-        <p class="content dark-background-text bullet-style text-spaced">
-          I did work
-        </p>
-      </div>
-    </section>
-
-    <div class="container">&nbsp;</div>
-
-    <section class="dark-background dark-background-section columns">
-      <div class="container column is-one-third">
-        <h1 class="green-underline-text is-size-3 text-spaced">
-          Fort Hays State
-        </h1>
-        <h2 class="rose-text is-size-4">
-          &nbsp;&nbsp;&nbsp;&nbsp;MGMT Information Systems
-        </h2>
-        <p>
-          <span class="is-size-4">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>2016 to Present (4 years)
-        </p>
-      </div>
-      <div class="container column">
-        <div class="container">&nbsp;</div>
-        <p class="content dark-background-text bullet-style text-spaced">
-          I did work
-        </p>
-      </div>
-    </section>
 
   </div>
 </template>
@@ -283,6 +313,20 @@ strong {
 .green-underline-text {
   text-decoration-line: underline;
   text-decoration-color: rgb(86, 255, 208);
+}
+
+.hover-rose-underline-text {
+  letter-spacing: .05em;
+  /* text-decoration-color: rgb(86, 255, 208); */
+  font-size: 125%;
+}
+.hover-rose-underline-text:hover {
+  font-size: 155%;
+  letter-spacing: .10em;
+  padding-bottom: 2em;
+  text-decoration-line: underline overline;
+  text-decoration-color: rgb(241, 72, 106);
+
 }
 
 .rose-overline {
